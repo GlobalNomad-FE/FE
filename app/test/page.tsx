2bb24969e4d2bac2ff03b8282aa27a00c0ae0967
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-05-22 14:03:42 +0900

    feat: css 없는 gnb

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,35 +1,11 @@
 'use client'; // app라우터에서는 맨위에 이거 써야 훅 쓸수 있는것 같습니다
 
-import ModalBox from '@/components/commons/ModalBox';
-import useModalStore from '@/libs/modalStore';
+import Gnb from '@/components/commons/gnb/gnb';
 
 export default function page() {
-  const { openModal, setOpenModal } = useModalStore();
-  const handleOpenTestModal = () => {
-    setOpenModal('openTestModal');
-  };
-
-  const handleOpenTest2Modal = () => {
-    setOpenModal('openTest2Modal');
-  };
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <button
-          className="mb-3 text-5xl p-5 rounded-lg bg-violet200"
-          onClick={handleOpenTestModal}
-        >
-          테스트모달버튼
-        </button>
-        <button
-          className="mb-3 text-5xl p-5 rounded-lg bg-violet200"
-          onClick={handleOpenTest2Modal}
-        >
-          테스트2모달버튼
-        </button>
-      </div>
-      {openModal && <ModalBox />}
-    </main>
+    <div>
+      <Gnb />
+    </div>
   );
 }
--- components/commons/gnb/gnb.tsx
+++ components/commons/gnb/gnb.tsx
@@ -2,7 +2,7 @@ import { useOutsideClick } from '@/hooks/useOutsideClick';
 import { useToggleButton } from '@/hooks/useToggleButton';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import Avatar from '@/components/avatar/avatar';
 import DropdownMenu from '../dropdownMenu/DropdownMenu';
@@ -18,18 +18,31 @@ export default function Gnb() {
   } = useToggleButton();
   const ref = useRef<HTMLButtonElement>(null);
 
+  // 외부 클릭을 감지하여 드롭다운 메뉴를 닫습니다.
   useOutsideClick(ref, isDropdownOpen, isDropdownOpenToggle);
 
+  useEffect(() => {
+    // 로그인 상태를 확인하고 설정합니다.
+    if (
+      localStorage.getItem('accessToken') &&
+      localStorage.getItem('refreshToken')
+    ) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, []);
+
   const handleLogout = () => {
+    // 로그아웃 시 처리하는 함수입니다.
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
-    if (!localStorage.getItem('accessToken')) {
-      isDropdownOpenToggle();
-      router.push('/signin');
-    }
+    isDropdownOpenToggle();
+    router.push('/signin');
   };
 
   const handleMyPageClick = () => {
+    // 마이 페이지로 이동하는 함수입니다.
     isDropdownOpenToggle();
     router.push('/mypage');
   };
@@ -64,14 +77,13 @@ export default function Gnb() {
             </div>
           ) : (
             <div>
-              <button>
+              <button onClick={isNotificationOpenToggle}>
                 <Image
                   src="/icons/notification.svg" // /public 제거
                   alt="알림"
-                  onClick={isNotificationOpenToggle}
                 />
               </button>
-              {isNotificationOpen && '모달 들어갈 자리'}
+              {isNotificationOpen && '모달 자리'}
               <div>
                 <div />
                 <div>
@@ -79,7 +91,9 @@ export default function Gnb() {
                     profileImageUrl={null} // profileImageUrl 변수 정의 필요
                     type="gnb"
                   />
-                  <button onClick={isDropdownOpenToggle} ref={ref}></button>
+                  <button onClick={isDropdownOpenToggle} ref={ref}>
+                    메뉴
+                  </button>
                   {isDropdownOpen && (
                     <DropdownMenu type="gnb" dropdownMenuList={MyMenuList} />
                   )}
