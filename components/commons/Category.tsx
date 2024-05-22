6b80ccd1ee4af996eb01616000b83f6de0566bc9
gaem-gaem <kkmm080399@gmail.com>
2024-05-22 17:08:55 +0900

    refactor: Category 컴포넌트 rounded 값 px로 변경

--- components/commons/Category.tsx
+++ components/commons/Category.tsx
@@ -15,7 +15,7 @@ interface CategoryType {
 const Category = ({ onClick, isSelected, children }: CategoryType) => {
   return (
     <div
-      className={`flex justify-center items-center w-[12.7rem] h-[5.3rem] border border-solid border-green200 rounded-3xl font-family text-[1.8rem] font-medium cursor-pointer ${
+      className={`flex justify-center items-center w-[12.7rem] h-[5.3rem] border border-solid border-green200 rounded-[15px] font-family text-[1.8rem] font-medium cursor-pointer ${
         isSelected ? 'bg-nomad-black text-white' : ''
       } tablet:w-[12rem] mobile:w-[8rem] mobile:h-[4.1rem] mobile:text-[1.4rem]`}
       onClick={onClick}
