00fb3a95dc25163dd0e31addd6b591a9eee423c3
seungcar <ysjysj777@naver.com>
2024-05-24 10:11:40 +0900

    style: 팝업박스 리모델링

--- app/testPopup/page.tsx
+++ app/testPopup/page.tsx
@@ -1,39 +1,44 @@
 'use client'; // app라우터에서는 맨위에 이거 써야 훅 쓸수 있는것 같습니다
 
-import React from 'react';
-import usePopupStore from '@/libs/popupStore';
-import PopupBox from '@/components/commons/PopupBox';
+import BasePopup from '@/components/commons/BasePopup';
+import React, { useState } from 'react';
 
 export default function Page() {
-  const { openModalIfTextExists, setOpenModalIfTextExists } = usePopupStore();
+  const [openPopup, setOpenPopup] = useState(false);
+  const [popupText, setPopupText] = useState('');
+  const [buttonNum, setButtonNum] = useState(1);
 
-  const handleOpenPopup = (message: string) => {
-    setOpenModalIfTextExists(message);
+  const signupFinishedPopup = () => {
+    setOpenPopup(true);
+    setPopupText('가입이 완료되었습니다!');
+    setButtonNum(1);
   };
 
-  const buttons = [
-    { text: '가입완료', onClick: () => handleOpenPopup('가입이 완료되었습니다!') },
-    { text: '예약취소', onClick: () => handleOpenPopup('예약을 취소하시겠어요?') },
-    { text: '비밀번호 불일치', onClick: () => handleOpenPopup('비밀번호가 일치하지 않습니다.') },
-    { text: '사용중인이메일', onClick: () => handleOpenPopup('이미 사용중인 이메일입니다.') },
-    { text: '예약완료', onClick: () => handleOpenPopup('예약이 완료되었습니다.') },
-    { text: '체험등록완료', onClick: () => handleOpenPopup('체험 등록이 완료되었습니다') },
-  ];
+  const cancelReservationPopup = () => {
+    setOpenPopup(true);
+    setPopupText('예약을 취소하시겠어요?');
+    setButtonNum(2);
+  };
+
+  const closePopup = () => {
+    setOpenPopup(false);
+  };
 
   return (
-    <main className="p-[5rem]">
-      <div className="grid grid-cols-8 gap-6">
-        {buttons.map((button, index) => (
-          <button
-            key={index}
-            className="text-body1-regular p-1 rounded-[8px] bg-green200"
-            onClick={button.onClick}
-          >
-            {button.text}
-          </button>
-        ))}
-      </div>
-      {openModalIfTextExists && <PopupBox />}
+    <main className="p-[5rem] text-white flex justify-center gap-20">
+      <button
+        className="text-body1-regular p-1 rounded-[8px] bg-green200 w-64 h-16"
+        onClick={signupFinishedPopup}
+      >
+        가입완료
+      </button>
+      <button
+        className="text-body1-regular p-1 rounded-[8px] bg-green200 w-64 h-16"
+        onClick={cancelReservationPopup}
+      >
+        예약취소
+      </button>
+      {openPopup && <BasePopup text={popupText} numOfButtons={buttonNum} closePopup={closePopup}/>}
     </main>
   );
 }
