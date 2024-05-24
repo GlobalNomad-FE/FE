3ffed5cf4ee8631d2a64d135b318a7a678893123
janyongs <wldud5023@naver.com>
2024-05-24 14:25:18 +0900

    [#27] 메인페이지에서 테스트 한거 되돌려놓기

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 'use client';
-import FloatingBoxContainerSelector from '@/components/FloatingBox/FloatingBoxContainerSelector';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FloatingBoxContainerSelector />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
