6a2317a67528f435162a5cbd614716d3a129a110
janyongs <wldud5023@naver.com>
2024-05-24 14:10:57 +0900

    [#27] feat: 캘린더 반응형구현 - desktop/ tablet 크기별 캘린더컴포넌트 구분

--- components/FloatingBox/DatePicker.tsx
+++ components/FloatingBox/DatePicker.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Calendar from '../commons/calendar/Calender';
+import React, { useState } from 'react';
+import Calender from '../commons/calendar/Calender';
 import TabletCalender from './Calender/TabletCalender';
 import { useMediaQuery } from 'react-responsive';
 
 const DatePicker = () => {
   const isDesktopOrTablet = useMediaQuery({ minWidth: 1024 });
+
   return (
     <div className="border-t border-t-gray200 mt-4">
       <p className="text-h3-bold text-black200 mt-4 mb-4">날짜</p>
       {/* pc-Calendar컴포넌트 , tablet-TabletCalender컴포넌트 */}
-      {isDesktopOrTablet ? <Calendar /> : <TabletCalender />}
+      {isDesktopOrTablet ? <Calender /> : <TabletCalender />}
     </div>
   );
 };
