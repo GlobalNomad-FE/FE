8a93aa6dcdfa294c8ef194998bf6afd72eac1338
janyongs <wldud5023@naver.com>
2024-05-25 21:07:52 +0900

    [#45] feat : ReservationWidget 안에서 공통컴포넌트 나눔.

---
+++ components/reservationWidget/common/ReservationWidgetWraaper.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ReservationWidgetWraaper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className="relative flex flex-col w-96 bg-white border rounded-2xl border-gray200 p-6 tablet:w-[251px]"
+      style={{ boxShadow: '0px 4px 16px rgba(17, 34, 17, 0.05)' }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default ReservationWidgetWraaper;
---
+++ components/reservationWidget/common/TimeSelector.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useCalendar } from '../lib/Calendar.provider';
+
+/**
+ *
+ * @param schedules: api 데이터로 받아오는 스케쥴 리스트 (프로바이더 schedule과 다른것)
+ */
+const TimeSelector = ({ schedules }: { schedules: any[] }) => {
+  const { onChangeSchedule, selectSchedule, formatedDate } = useCalendar();
+
+  return (
+    <div className="mt-4  flex flex-col gap-3.5 mobile:mt-7">
+      <p className="text-h3-bold text-black200 mobile:text-h2">
+        예약 가능한 시간
+      </p>
+      <div>
+        {schedules.map((data) => {
+          const isSelected = selectSchedule?.id === data.id;
+          const isDate = formatedDate === data.date;
+          return (
+            isDate && (
+              <button
+                className={`border rounded-lg border-nomad-black active:bg-nomad-black active:text-white text-body1-regular font-medium-w py-[1rem] px-[1.2rem] text-nomad-black mr-[1.2rem] mb-[1.2rem] ${
+                  isSelected
+                    ? 'bg-nomad-black text-white'
+                    : 'bg-white text-nomad-black'
+                }`}
+                key={data.id}
+                onClick={() => onChangeSchedule(data)}
+              >
+                {data.startTime}~{data.endTime}
+              </button>
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TimeSelector;
