a81f10909c85efcdc9d40a531410982d78f85117
seungcar <ysjysj777@naver.com>
2024-05-27 22:07:10 +0900

    style: 버튼 공통컴포넌트 수정

--- components/commons/Button.tsx
+++ components/commons/Button.tsx
@@ -10,18 +10,17 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   width: number;
   height: number;
   fontSize: number;
+  textBold?: boolean;
   btnColor: 'green' | 'white' | 'gray';
   textColor: 'green' | 'white' | 'gray';
   border?: boolean;
   borderColor?: 'green' | 'white' | 'gray';
-  rounded? : number;
+  rounded?: number;
   hover?: boolean;
   clicked?: boolean;
   children: React.ReactNode;
 }
 /**
- * @param {green' | 'white' | 'gray} btnColor - 버튼의 색상
- * @param {green' | 'white' | 'gray} textColor - 버튼 내부 텍스트 색상
  * @param {boolean} border - 테두리 유무, 기본값 false
  * @param {green' | 'white' | 'gray} borderColor - 테두리 색상, 기본값 green
  * @param {number} rounded - 테두리 굴곡 정도, 기본값 8px
@@ -32,6 +31,7 @@ const Button = ({
   width,
   height,
   fontSize,
+  textBold = false,
   btnColor,
   textColor,
   border = false,
@@ -46,7 +46,7 @@ const Button = ({
   const pxToRem = (value: number) => {
     return value / 16 + 'rem';
   };
-  
+
   const handleMouseOver = () => {
     if (hover) setHovered(true);
   };
@@ -62,22 +62,21 @@ const Button = ({
         onMouseEnter={handleMouseOver}
         onMouseLeave={handleMouseOut}
         style={{
-          width: pxToRem(hovered || clicked? width * 0.8 : width),
-          height: pxToRem(hovered || clicked? height * 0.8 : height),
-          fontSize: pxToRem(hovered || clicked? fontSize * 1.2 : fontSize),
+          width: pxToRem(hovered || clicked ? width * 0.8 : width),
+          height: pxToRem(hovered || clicked ? height * 0.8 : height),
+          fontSize: pxToRem(hovered || clicked ? fontSize * 1.2 : fontSize),
+          fontWeight: textBold ? 700 : 400,
           backgroundColor: ColorValue[btnColor],
           color: ColorValue[textColor],
-          marginTop: pxToRem(hovered || clicked? height * 0.1 : 0),
-          marginRight: pxToRem(hovered || clicked? width * 0.1 : 0),
-          marginBottom: pxToRem(hovered || clicked? height * 0.1 : 0),
-          marginLeft: pxToRem(hovered || clicked? width * 0.1 : 0),
+          marginTop: pxToRem(hovered || clicked ? height * 0.1 : 0),
+          marginRight: pxToRem(hovered || clicked ? width * 0.1 : 0),
+          marginBottom: pxToRem(hovered || clicked ? height * 0.1 : 0),
+          marginLeft: pxToRem(hovered || clicked ? width * 0.1 : 0),
           borderRadius: `${rounded}px`,
-          border: border
-          ? `solid 1px ${ColorValue[borderColor]}`
-          : 'none',
-          cursor: hovered ? 'pointer' : 'default', 
+          border: border ? `solid 1px ${ColorValue[borderColor]}` : 'none',
+          cursor: hovered ? 'pointer' : 'default',
         }}
-        className='cursor-pointer flex items-center justify-center duration-500'
+        className="cursor-pointer flex items-center justify-center duration-500"
       >
         {children}
       </button>
