333016ef052196211c07e2e5af65795ca1f086fa
janyongs <wldud5023@naver.com>
2024-05-27 22:16:39 +0900

    [#54] feat : 상세페이지 기본 Ui틀 구현

--- app/activities/detail/page.tsx
+++ app/activities/detail/page.tsx
@@ -1,15 +1,30 @@
-import Image from 'next/image';
 import React from 'react';
 import Gnb from '@/components/commons/gnb/gnb';
-import ActivitieTitle from '@/components/activitieTitile/ActivitieTitle';
-// import StarIcon from '@/public/icons/Star.svg';
+import ActivitieTitle from '@/components/activitie/activitieTitile/ActivitieTitle';
+import ReservationWidgetContainerSelector from '@/components/reservationWidget/ReservationWidgetContainerSelector';
+import { CalendarProvider } from '@/components/reservationWidget/lib/Calendar.provider';
+import data from '@/components/reservationWidget/mock.json';
+import ActivitieBio from '@/components/activitie/ActivitieBio';
+import Map from '@/components/activitie/Map';
+import Review from '@/components/activitie/Review';
 
 export default function ActivitiesDetailPage() {
+  const { description } = data;
   return (
-    <main>
+    <main className="max-w-[1200px] mx-auto px-[24px]">
       <Gnb />
-      <div className="mx-auto">
-        <ActivitieTitle />
+      <ActivitieTitle />
+      <div className="flex gap-5 mt-[50px]">
+        <div className="w-[790px] flex flex-col">
+          <ActivitieBio description={description} />
+          <Map location={data.address} />
+          <Review data={data} />
+        </div>
+        <div className="">
+          <CalendarProvider>
+            <ReservationWidgetContainerSelector />
+          </CalendarProvider>
+        </div>
       </div>
     </main>
   );
