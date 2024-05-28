0b1bebcd267af2a750d167f4d7f93e4de1e81d60
seungcar <ysjysj777@naver.com>
2024-05-28 18:25:30 +0900

    feat: 후기작성 모달 반응형추가

--- components/commons/Popups/ReviewModal/ReviewForm.tsx
+++ components/commons/Popups/ReviewModal/ReviewForm.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import Button from '@/components/commons/Button';
 import Rating from './Rating';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 const ReviewForm = () => {
+  const isMobile = useMediaQuery('(max-width: 767px)');
+
   return (
     <>
       <Rating />
       <form
-        className="flex flex-col w-full gap-[24px]"
+        className={`flex flex-col w-full  ${
+          isMobile ? 'gap-[12px]' : 'gap-[24px]'
+        }`}
         // onSubmit={handleSubmit}
       >
         <textarea
-          className="w-full h-[240px] px-[16px] py-[8px] border border-gray-500 rounded-[4px] text-body1-regular"
+          className={`w-full px-[16px] py-[8px] border border-gray-500 rounded-[4px] text-body1-regular ${
+            isMobile ? 'h-[346px]' : 'h-[240px]'
+          }`}
           placeholder="후기를 작성해주세요"
         />
         <Button
           type="submit"
           width={'full'}
-          height={56}
+          height={isMobile ? 48 : 56}
           fontSize={16}
           textBold={true}
           btnColor={'green'}
