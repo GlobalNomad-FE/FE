67e716c45906d3dc34b89ccd5f9cd456358bb088
gaem-gaem <kkmm080399@gmail.com>
2024-05-28 19:46:46 +0900

    feat: 예약 내역 페이지 예약 없을 때 UI 구현

--- app/my-reservations/page.tsx
+++ app/my-reservations/page.tsx
@@ -1,9 +1,10 @@
 'use client';
+import { useState } from 'react';
+import Gnb from '@/components/commons/gnb/gnb';
 import SideNavigationMenu from '@/components/commons/SideNavigationMenu';
 import FilterDropdown from '@/components/commons/FilterDropdown';
 import data from './mock.json';
-import { useState } from 'react';
-import Gnb from '@/components/commons/gnb/gnb';
+import Image from 'next/image';
 
 const MyReservations = () => {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
@@ -18,24 +19,43 @@ const MyReservations = () => {
       <main className="flex justify-center min-h-[100vh] max-h-[100%] bg-gray50">
         <div className="flex gap-6 w-[1200px] pt-[142px]">
           <SideNavigationMenu />
-          <div className="flex flex-col gap-6 flex-grow">
+          <div className="flex flex-col flex-grow">
             <div className="flex justify-between">
               <p className="text-[2rem] font-bold">예약내역</p>
-              <FilterDropdown type="bookingPage" onSelect={handleSelect} />
+              {data.totalCount !== 0 && (
+                <FilterDropdown type="bookingPage" onSelect={handleSelect} />
+              )}
             </div>
-            {data.reservations.map((item) => (
-              <div
-                key={item.id}
-                className="flex w-[792px] min-h-[204px] bg-white"
-              >
-                <div className="w-[204px] h-[100%] bg-gray200"></div>
-                <div>
-                  <p>{item.activity.title}</p>
-                  <p>{item.date}</p>
-                  <p>₩{item.totalPrice}</p>
-                </div>
+            {data.totalCount === 0 ? (
+              <div className="flex flex-col flex-grow gap-5 items-center mt-[90px]">
+                <Image
+                  src="/icons/empty.svg"
+                  className="my-[31px] mx-[55px]"
+                  width={130}
+                  height={177}
+                  alt="빈 예약 내역"
+                />
+                <p className="text-[1.5rem] font-medium text-gray500">
+                  아직 등록한 체험이 없어요
+                </p>
               </div>
-            ))}
+            ) : (
+              <div className="mt-[16px]">
+                {data.reservations.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex w-[792px] min-h-[204px] bg-white"
+                  >
+                    <div className="w-[204px] h-[100%] bg-gray200"></div>
+                    <div>
+                      <p>{item.activity.title}</p>
+                      <p>{item.date}</p>
+                      <p>₩{item.totalPrice}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </main>
