1148443816bc082aa4a26d986f587008c9b16d95
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-05-29 22:54:23 +0900

    feat: card Resource 만들기

--- app/testSeung/page.tsx
+++ app/testSeung/page.tsx
@@ -1,28 +1,13 @@
-'use client'; // app라우터에서는 맨위에 이거 써야 훅 쓸수 있는것 같습니다
-
-import CardResource from '@/components/commons/CardResource';
-
-//임시 데이터
-
-const activity = {
-  address: '123 Main St, Anytown, USA',
-  bannerImageUrl: 'https://example.com/banner1.jpg',
-  category: 'Outdoor Activities',
-  createdAt: '2024-05-28T12:00:00Z',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  id: 1,
-  price: 10000,
-  rating: 4.5,
-  reviewCount: 50,
-  title: 'Hiking Adventure',
-  updatedAt: '2024-05-28T12:00:00Z',
-  userId: 1,
-};
+import {
+  CardResource,
+  CardResourceCategory,
+} from '@/components/commons/CardResource';
 
 export default function page() {
   return (
     <div>
-      <CardResource activitiesData={activity} banner={false} />
+      <CardResource />
+      <CardResourceCategory />
     </div>
   );
 }
--- components/commons/CardResource.tsx
+++ components/commons/CardResource.tsx
@@ -1,80 +1,77 @@
 'use client';
 
+import React from 'react';
 import Image from 'next/image';
-import { GetActivitiesList } from '@/types/activities';
-import { useRouter } from 'next/navigation';
 
-interface CardResourceProps {
-  activitiesData: GetActivitiesList;
-  banner: boolean;
-}
-
-// Mock 데이터
-const mockActivitiesData: GetActivitiesList[] = [
-  {
-    address: '123 Main St, Anytown, USA',
-    bannerImageUrl: 'https://example.com/banner1.jpg',
-    category: 'Outdoor Activities',
-    createdAt: '2024-05-28T12:00:00Z',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    id: 1,
-    price: 10000,
-    rating: 4.5,
-    reviewCount: 50,
-    title: 'Hiking Adventure',
-    updatedAt: '2024-05-28T12:00:00Z',
-    userId: 1,
-  },
-  // Add more mock data as needed
-];
-
-export default function CardResource({
-  activitiesData,
-  banner,
-}: CardResourceProps) {
-  const router = useRouter();
-
-  // Find activity data by activityId
-  const activityData = mockActivitiesData.find(
-    (activity) => activity.id === activitiesData.id,
-  );
-
-  if (!activityData) {
-    return <div>Activity not found</div>;
-  }
-
-  const handleClick = () => {
-    router.push(`/activityDetail/${activitiesData.id}`);
-  };
-
+export const CardResource = () => {
   return (
-    <div onClick={handleClick}>
-      <div>
-        <Image src="" width={384} height={384} alt="배너 이미지" />
-      </div>
-      <div>
-        <div>
+    <div className="relative h-[186px] w-[186px] overflow-hidden rounded-[20px] md:h-[384px] md:w-[384px]">
+      <Image
+        className="absolute z-0 scale-125 rounded-[20px] object-cover"
+        fill
+        src="/images/Banner.png"
+        alt="배너"
+      />
+      <div className="from-0.1% absolute z-[1] h-full w-full rounded-[20px] bg-gradient-to-t from-black/80"></div>
+      <div className="absolute bottom-[8px] z-[1] flex w-[186px] flex-col gap-[6px] px-[20px] py-[12px] text-[#ffffff] md:bottom-0 md:w-[384px] md:gap-[20px] md:py-[30px]">
+        <div className="flex gap-[5px] text-[#ffffff]">
           <Image
             src="/icons/star-on.svg"
             width={20}
             height={20}
-            alt="별모양 아이콘"
+            alt="star icon"
           />
-          {activitiesData.rating}
-          &nbsp;
-          <span> ({activitiesData.reviewCount})</span>
+          <span className="text-[14px] font-[600] text-[#ffffff]">
+            4.9 (793)
+          </span>
         </div>
-        <div>{activitiesData.title}</div>
-        <div>
-          {activitiesData.price === 0 ? (
-            '무료체험'
-          ) : (
-            <>
-              ￦ {activitiesData.price.toLocaleString()} <span>/ 인</span>
-            </>
-          )}
+        <div className="line-clamp-2 w-[146px] break-keep text-[18px] font-[700] md:w-[230px] md:text-[30px] text-[#ffffff]">
+          함께 배우면 즐거운 스트릿 댄스
+        </div>
+        <div className="flex gap-[5px] text-[16px] font-[700] md:text-[20px] text-[#ffffff]">
+          <span className="text-[#ffffff]">₩ 38,000</span>
+          <span className="self-center text-[14px] font-[400] text-[#a1a1a1]">
+            / 인
+          </span>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export const CardResourceCategory = () => {
+  return (
+    <div className="flex w-min flex-col gap-[16px] text-[#ffffff]">
+      <div className="relative h-[168px] w-[168px] overflow-hidden rounded-[20px] md:h-[221px] md:w-[221px] xl:h-[283px] xl:w-[283px]">
+        <Image
+          className="scale-125 rounded-[20px] object-cover"
+          fill
+          src="/images/Banner.png"
+          alt="image"
+        />
+        <div className="absolute z-[1] flex w-[168px] flex-col gap-[10px] text-[#1b1b1b] md:w-[282px]">
+          <div className="flex gap-[5px] text-[#ffffff]">
+            <Image
+              src="/icons/star-on.svg"
+              width={20}
+              height={20}
+              alt="star icon"
+            />
+            <span className="text-[16px] font-[500] text-[#ffffff]">
+              4.9 <span className="text-[#a1a1a1]">(793)</span>
+            </span>
+          </div>
+          <div className="pb-[5px] text-[18px] font-[600] md:text-[24px] text-[#ffffff]">
+            함께 배우면 즐거운 스트릿 댄스
+          </div>
+          <div className="flex gap-[5px] text-[20px] font-[700] md:text-[28px] text-[#ffffff]">
+            <span className="text-[#ffffff]">₩ 38,000</span>
+            <span className="self-center text-[16px] font-[400] text-[#4b4b4b] md:text-[20px]">
+              / 인
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
