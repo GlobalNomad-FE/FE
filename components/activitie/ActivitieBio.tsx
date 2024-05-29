c0efb066abfd8ead4d23cbe14199b826fbc97fd7
janyongs <wldud5023@naver.com>
2024-05-29 22:50:06 +0900

    fix: 타입 추가

--- components/activitie/ActivitieBio.tsx
+++ components/activitie/ActivitieBio.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ActivitieBio({ description }) {
+export default function ActivitieBio({ description }: { description: string }) {
   return (
     <div className="border-t border-gray200 py-[40px] flex flex-col gap-[16px]">
       <p className="text-h3-bold text-nomad-black">체험 설명</p>
