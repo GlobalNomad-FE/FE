5fd05b0954094d6d101ab905b84cdafd6afa00a0
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-05-29 13:19:44 +0900

    style: footer 수정

--- components/commons/Footer.tsx
+++ components/commons/Footer.tsx
@@ -4,38 +4,50 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="flex bottom-[0] h-40 fixed justify-between items-center content-center pt-8 pb-16 px-[104px] bg-nomad-black w-full">
-      <div className="flex gap-64">
-        <span className="text-gray500">©codeit - 2023</span>
-        <div className="flex items-center gap-3">
-          <span className="text-gray500">Privacy Policy</span>
-          <span className="text-gray500">FAQ</span>
+    <footer className="bottom-0 flex h-24 w-full min-w-[400px] items-center justify-between whitespace-nowrap border-none bg-nomad-black p-0 px-20 text-white mobile:px-0">
+      <div className="flex w-full items-center justify-between justify-items-center pb-6 mobile:flex-col">
+        <div className="flex w-3/5 justify-between gap-4">
+          <div className=" text-gray600 "> @codeit - 2024 </div>
+          <div className="flex gap-5 whitespace-nowrap ">
+            <div className=" text-gray600">Privacy Policy</div>
+            <div className=" text-gray600">FAQ</div>
+          </div>
+        </div>
+        <div className="flex items-start gap-3">
+          <Link href="https://facebook.com/">
+            <Image
+              src="/icons/facebook.svg"
+              height={20}
+              width={20}
+              alt="페이스북"
+            />
+          </Link>
+          <Link href="https://twitter.com/">
+            <Image
+              src="/icons/twitter.svg"
+              height={20}
+              width={20}
+              alt="트위터"
+            />
+          </Link>
+          <Link href="https://youtube.com/">
+            <Image
+              src="/icons/youtube.svg"
+              height={20}
+              width={20}
+              alt="유튜브"
+            />
+          </Link>
+          <Link href="https://instagram.com/">
+            <Image
+              src="/icons/instagram.svg"
+              height={20}
+              width={20}
+              alt="인스타그램"
+            />
+          </Link>
         </div>
       </div>
-      <div className="flex items-start gap-3">
-        <Link href="https://facebook.com/">
-          <Image
-            src="/icons/facebook.svg"
-            height={20}
-            width={20}
-            alt="페이스북"
-          />
-        </Link>
-        <Link href="https://twitter.com/">
-          <Image src="/icons/twitter.svg" height={20} width={20} alt="트위터" />
-        </Link>
-        <Link href="https://youtube.com/">
-          <Image src="/icons/youtube.svg" height={20} width={20} alt="유튜브" />
-        </Link>
-        <Link href="https://instagram.com/">
-          <Image
-            src="/icons/instagram.svg"
-            height={20}
-            width={20}
-            alt="인스타그램"
-          />
-        </Link>
-      </div>
-    </div>
+    </footer>
   );
 }
