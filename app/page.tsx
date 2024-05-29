ef49e86acf6ada63e3555eaed1396c1bf943525d
seungcar <ysjysj777@naver.com>
2024-05-29 18:01:05 +0900

    style: gnb렌더링부분 코드 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Gnb from "@/components/commons/gnb/gnb";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Gnb></Gnb>
+      <Gnb />
     </main>
   );
 }
