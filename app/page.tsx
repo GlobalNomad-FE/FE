449e3249c05c815abe903c4227cbf9c022eda89e
seungcar <ysjysj777@naver.com>
2024-05-29 12:44:51 +0900

    style: gnb css 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Gnb from "@/components/commons/gnb/gnb";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Gnb></Gnb>
+    </main>
   );
 }
