0b2a3d87e1cba630c34282a6949532b10afb2afe
janyongs <wldud5023@naver.com>
2024-05-29 17:05:03 +0900

    [#67] feat : 후기 컴포넌트& 안에 후기 목록들 나타내는 테이블 컴포넌트

--- components/activitie/Review.tsx
+++ components/activitie/Review.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
+import ReviewTable from '../ReviewTable';
+import data from '@/components/reviewmock.json';
 
-export default function Review({ data }) {
+export default function Review() {
   return (
-    <div className="border-t border-gray200 py-[40px]">
+    <div className="border-t border-gray200 py-[40px] flex flex-col gap-6">
       <p className="text-h3-bold text-nomad-black">후기</p>
       <div className="flex gap-4 items-center">
         <p className="text-[50px] text-nomad-black font-semibold leading-normal ">
-          {data.rating}
+          {data.averageRating}
         </p>
         <div className="flex flex-col gap-[8px]">
           <p>매우 만족</p>
@@ -19,11 +21,12 @@ export default function Review({ data }) {
               height={18}
             />
             <p className="text-body2-regular text-nomad-black">
-              {data.reviewCount}개 후기
+              {data.totalCount}개 후기
             </p>
           </div>
         </div>
       </div>
+      <ReviewTable data={data} />
     </div>
   );
 }
