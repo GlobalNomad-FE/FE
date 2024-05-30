b7be9429b35d67901f259bbb59ba542485e41ae7
janyongs <wldud5023@naver.com>
2024-05-30 22:13:20 +0900

    [#79] fix : 외부클릭 함수 수정

--- components/activitie/Menu.tsx
+++ components/activitie/Menu.tsx
@@ -3,27 +3,23 @@ import Image from 'next/image';
 import { useState } from 'react';
 import { useRef } from 'react';
 import CancelReservation from '../commons/Popups/CancelReservation';
+import { useRouter, usePathname } from 'next/navigation';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
 
 export default function Menu() {
   const [isKebabOpen, setIsKebabOpen] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const optionsRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const pathname = usePathname();
+  const ref = useRef<HTMLDivElement>(null);
 
   // 메뉴 열고 닫기
-  const handleKebabToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleKebabToggle = () => {
     setIsKebabOpen(!isKebabOpen);
   };
 
-  // 메뉴 밖에 부분 클릭시 닫히게 만드는 이벤트함수
-  const handleKebabClose = (e: React.FocusEvent<HTMLButtonElement>) => {
-    /** e.relatedTarget는 EventTarget | null이기 때문에 Node인지 확인해야한다! */
-    if (
-      !(e.relatedTarget instanceof Node) ||
-      !optionsRef.current?.contains(e.relatedTarget)
-    ) {
-      setIsKebabOpen(false);
-    }
-  };
+  // 외부 클릭을 감지하여 케밥 메뉴를 닫습니다.
+  useOutsideClick(ref, isKebabOpen, handleKebabToggle);
 
   const handleDeletePopupOpen = () => {
     setIsPopupOpen(!isPopupOpen);
@@ -31,7 +27,7 @@ export default function Menu() {
 
   return (
     <div className="relative">
-      <button onClick={handleKebabToggle} onBlur={handleKebabClose}>
+      <button onClick={handleKebabToggle}>
         <Image
           src="/icons/meatball.svg"
           width={40}
@@ -42,9 +38,20 @@ export default function Menu() {
       {isKebabOpen && (
         <div
           className="bg-white border border-gray200 rounded-md absolute right-0 w-40 z-[1]"
-          ref={optionsRef}
+          ref={ref}
         >
-          <div className="px-[46px] py-[18px] cursor-pointer text-h4-regular text-gray600 hover:bg-green400 hover:text-green200">
+          <div
+            className="px-[46px] py-[18px] cursor-pointer text-h4-regular text-gray600 hover:bg-green400 hover:text-green200"
+            onClick={() => {
+              router.push(
+                '/',
+                //   {
+                //   pathname: '/',
+                //   query: { pid: data.activityId } =>나중에 수정페이지로 activityId 넘겨줘야함
+                // }
+              );
+            }}
+          >
             수정하기
           </div>
           <div
@@ -55,9 +62,9 @@ export default function Menu() {
           </div>
         </div>
       )}
-      {isPopupOpen && (
+      {/* {isPopupOpen && (
         <CancelReservation>정말 삭제하시겠습니까?</CancelReservation>
-      )}
+      )} */}
     </div>
   );
 }
