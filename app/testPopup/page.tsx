5701f93b043b80424f9ff9c048300e381303f47a
seungcar <ysjysj777@naver.com>
2024-05-31 16:01:06 +0900

    feat: 2버튼컴포넌트 생성

--- app/testPopup/page.tsx
+++ app/testPopup/page.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import CancelReservation from '@/components/commons/Popups/CancelReservation';
-import ReservationCompleted from '@/components/commons/Popups/ReservationCompleted';
-import EmailExist from '@/components/commons/Popups/EmailExist';
+import EmailExist from '@/components/tests/EmailExist';
 import React from 'react';
 import ReviewModal from '@/components/commons/Popups/ReviewModal/ReviewModal';
+import ReservationCompleted from '@/components/tests/ReservationCompleted';
 
 export default function Page() {
+
   return (
     <main className="p-[5rem] text-white flex justify-center gap-20">
       <EmailExist />
       <ReservationCompleted />
-      <CancelReservation>예약을 취소하시겠어요?</CancelReservation>
       <ReviewModal />
-      
     </main>
   );
 }
