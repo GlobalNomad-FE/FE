d8ef334b15224505779d20dde7f449053f80671f
janyongs <wldud5023@naver.com>
2024-06-03 12:46:10 +0900

    [#93] feat: 체험관리페이지 기본구현

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import Button from '@/components/commons/Button';
+import SideNavigationMenu from '@/components/commons/SideNavigationMenu';
+import ExperienceManagement from '@/components/commons/card/ExperienceManagement';
+import data from '@/app/activities/mock.json';
+import Gnb from '@/components/commons/gnb/gnb';
+import Link from 'next/link';
 
 export default function page() {
-  return <div>체험등록페이지</div>;
+  const { activities } = data;
+  return (
+    <div>
+      <Gnb />
+      <div className="mt-[142px] flex gap-6 mx-6 justify-center mobile:justify-start  ">
+        <SideNavigationMenu />
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between max-w-[800px] ">
+            <p className="text-title text-black">내 체험 관리</p>
+            <Link href={'/'}>
+              <Button
+                width={120}
+                height={45}
+                fontSize={16}
+                textBold={true}
+                btnColor={'nomadBlack'}
+                textColor={'white'}
+                rounded={4}
+                style={{ cursor: 'pointer' }}
+              >
+                체험 등록하기
+              </Button>
+            </Link>
+          </div>
+          <div className="relative">
+            {activities.map((data) => (
+              <ExperienceManagement
+                key={data.id}
+                bannerImageUrl={data.bannerImageUrl}
+                rating={data.rating}
+                reviewCount={data.reviewCount}
+                title={data.title}
+                price={data.price}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
