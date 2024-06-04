0f3f62d6e9c75a87d88f415973f549060c4a8c7e
janyongs <wldud5023@naver.com>
2024-06-04 15:50:34 +0900

    [#97] feat: 셀렉트 박스컴포넌트 공통화& 스타일 수정

--- components/commons/Selectbox.tsx
+++ components/commons/Selectbox.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import Select, { components, DropdownIndicatorProps } from 'react-select';
+import Select, {
+  components,
+  DropdownIndicatorProps,
+  StylesConfig,
+} from 'react-select';
 import Image from 'next/image';
 
 interface Option {
@@ -10,6 +14,8 @@ interface Option {
 interface SelectBoxProps {
   options: Option[];
   onSelect: (value: string) => void;
+  width: string;
+  placeholder: string;
 }
 
 /**
@@ -18,9 +24,15 @@ interface SelectBoxProps {
  * @param onSelect: (value: string) => void , value에 선택한 값이 담깁니다.
  */
 
-const Selectbox: React.FC<SelectBoxProps> = ({ options, onSelect }) => {
+const Selectbox: React.FC<SelectBoxProps> = ({
+  options,
+  onSelect,
+  width,
+  placeholder,
+}) => {
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const inputClassName = `w-[${width}]`;
 
   const DropdownIndicator = (props: DropdownIndicatorProps<Option>) => {
     return (
@@ -28,26 +40,89 @@ const Selectbox: React.FC<SelectBoxProps> = ({ options, onSelect }) => {
         <Image
           src={`/icons/arrow_${menuIsOpen ? 'up' : 'down'}.svg`}
           alt="arrow icon"
-          width={48}
-          height={48}
+          width={24}
+          height={24}
         />
       </components.DropdownIndicator>
     );
   };
+  const colorStyles: StylesConfig<Option> = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: 'white',
+      boxShadow: 'none',
+      borderRadius: '4px',
+      borderColor: '#79747e',
+      padding: '4px 0px 4px 16px',
+      height: '56px',
 
+      ':hover': {
+        ...styles[':active'],
+        borderColor: '#112211',
+      },
+    }),
+    option: (styles) => {
+      return {
+        ...styles,
+        backgroundColor: 'white',
+        color: '#112211',
+        cursor: 'pointer',
+        height: '40px',
+        borderRadius: '6px',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: '#112211',
+          color: 'white',
+          cursor: 'pointer',
+        },
+        ':hover': {
+          ...styles[':active'],
+          backgroundColor: '#112211',
+          color: 'white',
+          cursor: 'pointer',
+        },
+      };
+    },
+    input: (styles) => {
+      return {
+        ...styles,
+        height: '40px',
+      };
+    },
+    menu: (styles) => {
+      return {
+        ...styles,
+        padding: '8px',
+      };
+    },
+    valueContainer: (styles) => {
+      return {
+        ...styles,
+        padding: '0px',
+      };
+    },
+    placeholder: (styles) => ({
+      ...styles,
+      color: '#a1a1aa',
+      fontSize: '16px',
+      fontWeight: '400',
+    }),
+  };
   return (
     <Select
-      className="w-[800px] h-[56px]"
       options={options}
       value={selectedOption}
       onChange={(option) => {
         setSelectedOption(option as Option);
         onSelect((option as Option).value);
       }}
-      placeholder="카테고리"
-      components={{ DropdownIndicator }}
+      styles={colorStyles}
+      placeholder={placeholder}
+      components={{ DropdownIndicator, IndicatorSeparator: () => null }}
       onMenuOpen={() => setMenuIsOpen(true)}
       onMenuClose={() => setMenuIsOpen(false)}
+      isSearchable={false}
     />
   );
 };
