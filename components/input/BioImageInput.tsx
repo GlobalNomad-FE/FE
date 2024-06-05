cbb74e7f7b4ea9763310a5386cb2982fad74aaba
janyongs <wldud5023@naver.com>
2024-06-05 22:44:17 +0900

    [#97] feat: 소개이미지 등록 컴포넌트

---
+++ components/input/BioImageInput.tsx
@@ -0,0 +1,45 @@
+import ImageInput from './ImageInput';
+import Image from 'next/image';
+import { FileInputProps } from './BannerImageInput';
+
+export default function BioImageInput({ files, setFiles }: FileInputProps) {
+  const handleDeleteFile = (fileName: string) => {
+    setFiles((prev) => prev.filter((file) => file.name !== fileName));
+  };
+
+  return (
+    <>
+      <div className="text-h2 text-black200">소개 이미지</div>
+      <div className="w-full flex gap-6 flex-wrap tablet:gap-4 mobile:gap-2">
+        <ImageInput files={files} setFiles={setFiles} type="bio" />
+        {files.map((file) => (
+          <div className="relative" key={file.name}>
+            <div className="w-[180px] h-[180px] rounded-3xl overflow-hidden relative">
+              <Image
+                src={URL.createObjectURL(file)}
+                fill
+                alt={file.name}
+                placeholder="blur"
+                blurDataURL={URL.createObjectURL(file)}
+              />
+            </div>
+            <div
+              className="absolute right-[-10px] top-[-15px] z-1 w-10 h-10 tablet:w-8 tablet:h-8 tablet:top-[-10px] mobile:w-6 mobile:h-6 mobile:right-[-5px] mobile:top-[-5px]"
+              onClick={() => handleDeleteFile(file.name)}
+            >
+              <Image
+                src="/icons/file-close-btn.svg"
+                alt="파일삭제버튼"
+                fill
+                style={{ opacity: '0.8' }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <p className="text-gray600 text-h4-regular">
+        * 이미지는 최대 4개까지 등록 가능합니다.
+      </p>
+    </>
+  );
+}
