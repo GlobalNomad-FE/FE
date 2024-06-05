cd7113a02d652b68f75d959d24abfb8f8bebe52b
seungcar <ysjysj777@naver.com>
2024-06-05 18:54:37 +0900

    feat: 주소 수정, 토큰 추가

--- apis/api.tsx
+++ apis/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://sp-taskify-api.vercel.app/4-13',
+  baseURL: 'https://sp-globalnomad-api.vercel.app/4-13',
 });
 
 export async function getActivities(page: number, size: number) {
@@ -10,6 +10,9 @@ export async function getActivities(page: number, size: number) {
       method: 'offset',
       page,
       size,
+    },
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQ2LCJ0ZWFtSWQiOiI0LTEzIiwiaWF0IjoxNzE3NTgwNjU0LCJleHAiOjE3MTc1ODI0NTQsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.LlYf4ydJOUCni1Hy9oy_jK7UoFT5F914ax7igY-jpfg`,
     },
   });
   return response.data;
