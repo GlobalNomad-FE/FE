800c2f1ee755a7a4b3f5d7aa3688115917704ae6
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-06-05 14:21:12 +0900

    feat: signUp 페이지 ui

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import Input from '@/components/commons/Input';
 import Link from 'next/link';
 import Image from 'next/image';
+import { ChangeEvent, useState } from 'react';
 
-const SignIn = () => {
+const SignUp = () => {
+  const [password, setPassword] = useState('');
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <div className="flex w-full flex-col items-center pt-40 ">
+    <div className="flex w-full flex-col items-center pt-20">
       <div>
         <Link href="/">
           <Image
@@ -15,28 +24,33 @@ const SignIn = () => {
           />
         </Link>
       </div>
-      <form className="mt-10 flex flex-col gap-6">
-        <Input type={'email'} />
-        <Input type={'password'} />
+      <form className="mt-10 flex flex-col gap-4">
+        <Input type="email" />
+        <Input type="text" placeholder="닉네임을 입력해 주세요" />
+        <Input type="password" onChange={handlePasswordChange} />
+        <Input
+          type="passwordCheck"
+          password={password}
+          placeholder="비밀번호 확인"
+        />
         <button
           type="submit"
-          className=" h-12 rounded-md bg-gray-400 text-base font-bold text-white 
-          "
+          className="h-12 rounded-md bg-gray-400 text-base font-bold text-white"
         >
-          로그인 하기
+          회원가입 하기
         </button>
       </form>
       <div className="mt-8 flex gap-2 text-base font-normal text-gray800">
-        <p>회원이 아니신가요?</p>
+        <p>이미 회원이신가요?</p>
         <Link
-          href="/signup"
+          href="/signin"
           className="text-base font-normal text-green200 underline"
         >
-          회원가입
+          로그인
         </Link>
       </div>
     </div>
   );
 };
 
-export default SignIn;
+export default SignUp;
--- components/commons/Input.tsx
+++ components/commons/Input.tsx
@@ -2,10 +2,13 @@
 import { useState, ChangeEvent } from 'react';
 import { z } from 'zod';
 import Image from 'next/image';
+import classNames from 'classnames';
 
 interface Props {
-  type: 'email' | 'password'; //email, password
-  password?: string; //비밀번호 확인의 경우 기존 비밀번호가 담김
+  type: 'email' | 'password' | 'text' | 'passwordCheck'; // email, password, text (nickname), passwordCheck
+  password?: string; // 비밀번호 확인의 경우 기존 비밀번호가 담김
+  placeholder?: string; // placeholder text
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void; // onChange 핸들러
 }
 
 // 이메일 스키마 정의
@@ -13,19 +16,34 @@ const EmailSchema = z.string().email();
 
 /**
  * 이메일과 비밀번호를 받는 인풋박스입니다.
- * @param type email, password
+ * @param type email, password, text (nickname), passwordCheck
  * @param password 비밀번호 확인의 경우 기존 비밀번호. 미존재시 null
+ * @param placeholder 인풋박스의 플레이스홀더 텍스트
+ * @param onChange 인풋박스의 onChange 핸들러
  * @returns Input
  */
-const Input = ({ type, password = '' }: Props) => {
+const Input = ({ type, password = '', placeholder = '', onChange }: Props) => {
   const [errorMessageColor, setErrorMessageColor] = useState('gray500');
-
-  const labelName = type === 'email' ? '이메일' : '비밀번호';
-  const placeholder =
-    type === 'email' ? '이메일을 입력해 주세요' : '비밀번호를 입력해주세요';
   const [errorMessage, setErrorMessage] = useState('');
 
+  const labelName =
+    type === 'email'
+      ? '이메일'
+      : type === 'password'
+      ? '비밀번호'
+      : type === 'passwordCheck'
+      ? '비밀번호 확인'
+      : '닉네임';
+
+  const defaultPlaceholder =
+    type === 'email'
+      ? '이메일을 입력해 주세요'
+      : type === 'password' || type === 'passwordCheck'
+      ? '비밀번호를 입력해주세요'
+      : '닉네임을 입력해주세요';
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (onChange) onChange(e);
     setErrorMessage('');
     setErrorMessageColor('gray500');
     if (type === 'email') {
@@ -38,16 +56,27 @@ const Input = ({ type, password = '' }: Props) => {
         setErrorMessageColor('red100');
       }
     }
+    if (type === 'passwordCheck' && password && password !== e.target.value) {
+      setErrorMessage('비밀번호가 일치하지 않습니다.');
+      setErrorMessageColor('red100');
+    }
   };
 
   const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length < 8 && type === 'password') {
+    if (
+      e.target.value.length < 8 &&
+      (type === 'password' || type === 'passwordCheck')
+    ) {
+      setErrorMessage('8자 이상 입력해주세요.');
       setErrorMessageColor('red100');
-      return setErrorMessage('8자 이상 입력해주세요');
+      return;
     }
-    //TODO : 비밀번호 확인 시 비밀번호가 다른 경우, 오류 발생
-    if (password && password !== e.target.value) {
+    if (type === 'passwordCheck' && password && password !== e.target.value) {
       setErrorMessage('비밀번호가 일치하지 않습니다.');
+      setErrorMessageColor('red100');
+    }
+    if (type === 'text' && e.target.value.length < 2) {
+      setErrorMessage('닉네임은 2글자 이상만 사용할 수 있습니다.');
       setErrorMessageColor('red100');
     }
   };
@@ -59,30 +88,43 @@ const Input = ({ type, password = '' }: Props) => {
   };
 
   return (
-    <label className="flex flex-col font-normal text-base gap-[8px] relative w-[560px] text-[16px] ">
+    <label className="flex flex-col font-normal text-base gap-[12.8px] relative w-[560px] text-[16px] ">
       {labelName}
       <input
         type={
-          type === 'email' || (type === 'password' && visibility)
+          type === 'email' ||
+          (type === 'password' && visibility) ||
+          (type === 'passwordCheck' && visibility)
             ? type
             : 'text'
         }
-        className={`w-[560px] h-12 text-[16px] px-5 py-4 bg-white rounded-md border border-${errorMessageColor}`}
+        className={classNames(
+          'w-[560px] h-12 text-[16px] px-5 py-4 bg-white rounded-md border',
+          {
+            'border-gray500': errorMessageColor === 'gray500',
+            'border-red100': errorMessageColor === 'red100',
+          },
+        )}
         onChange={handleChange}
         onBlur={handleBlur}
-        placeholder={placeholder}
+        placeholder={placeholder || defaultPlaceholder}
       />
-      {type === 'password' && (
+      {(type === 'password' || type === 'passwordCheck') && (
         <Image
           alt="비밀번호 눈 아이콘"
-          className="absolute pt-[44px] right-3 cursor-pointer "
+          className="absolute pt-[50px] right-3 cursor-pointer "
           src={`/icons/btn_visibility_${visibility ? 'off' : 'on'}.svg`}
           onClick={handleVisibility}
           width={24}
           height={24}
         />
       )}
-      <div className={`text-${errorMessageColor} text-[16px]`}>
+      <div
+        className={classNames('text-[16px]', {
+          'text-gray500': errorMessageColor === 'gray500',
+          'text-red100': errorMessageColor === 'red100',
+        })}
+      >
         {errorMessage}
       </div>
     </label>
