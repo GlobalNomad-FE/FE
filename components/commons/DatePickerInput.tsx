135f1e62553698adbce84d787dd9f7e2e703dcde
janyongs <wldud5023@naver.com>
2024-06-05 22:42:17 +0900

    [#97] feat: DateInput 수정

--- components/commons/DatePickerInput.tsx
+++ components/commons/DatePickerInput.tsx
@@ -1,8 +1,9 @@
 import DatePicker from 'react-datepicker';
 import Image from 'next/image';
 import { useState, useRef } from 'react';
-import { DateType } from './input/TimeInput';
+import { DateType } from '../input/TimeInput';
 import 'react-datepicker/dist/react-datepicker.css';
+import './DateInput.css';
 
 interface SelectDateProps {
   onSelectedDateChange: (date: DateType) => void; // 선택된 날짜 변경 시 호출될 콜백 함수
@@ -24,10 +25,10 @@ const DatePickerInput: React.FC<SelectDateProps> = ({
   };
 
   return (
-    <div className="relative w-[379px] h-[56px]">
+    <div className="relative w-full h-[56px]">
       <DatePicker
         ref={datePickerRef}
-        className="text-black200 border border-black100 rounded px-[16px] py-[8px] bg-white w-[379px] h-[56px] text-[16px]"
+        className="text-black200 border border-black100 rounded px-[16px] py-[8px] bg-white h-[56px] text-[16px] w-full"
         dateFormat="YY/MM/dd"
         selected={selectedDate}
         onChange={(date) => {
