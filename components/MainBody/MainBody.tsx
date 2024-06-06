007c4d08abdd8cd71e104c5d5688a0d07478272a
seungcar <ysjysj777@naver.com>
2024-06-06 09:52:55 +0900

    style: gnb z-index 수정

--- components/MainBody/MainBody.tsx
+++ components/MainBody/MainBody.tsx
@@ -1,12 +1,12 @@
 'use client';
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import SearchBar from '@/components/commons/SearchBar';
 import MonthBest from './MonthBest';
 import useGetActivities from '@/apis/activities/useGetActivities';
 import HotActivities from './HotActivities';
 
 const MainBody = () => {
-  // const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   // const page = 1;
   // const size = 2;
 
@@ -28,7 +28,11 @@ const MainBody = () => {
       <div className="absolute w-full flex justify-center top-[550px] z-30 text-black200 px-[24px] mobile:px-[16px] mobile:top-[250px]">
         <SearchBar onSearch={handleSearch} />
       </div>
-      <HotActivities />
+      <div className="w-full pt-[158px] mb-[120px] flex justify-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <HotActivities />
+        </Suspense>
+      </div>
     </div>
   );
 };
