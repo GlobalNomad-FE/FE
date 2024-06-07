5e1c7252e0bd68cefd06452f13f225251e2a7b5f
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-06-07 16:43:14 +0900

    어떻게 하면 좋을까...

--- apis/axios.ts
+++ apis/axios.ts
@@ -5,6 +5,21 @@ const instance = axios.create({
   baseURL: BASE_URL,
 });
 
+const getToken = () => {
+  // 브라우저(윈도우객체)가 있을때만 실행
+  if (typeof window !== undefined) {
+    const token = window.localStorage.getItem('accessToken');
+    return token;
+  }
+  return '';
+};
+
+instance.interceptors.request.use((config) => {
+  const modifiedConfig = { ...config };
+  modifiedConfig.headers.Authorization = `Bearer ${getToken()}`;
+  return modifiedConfig;
+});
+
 instance.interceptors.request.use((config) => {
   if (config.headers.Authorization) return config;
 
