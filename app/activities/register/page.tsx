ba46609d896d2eedefe36943adce373a24bb124c
janyongs <wldud5023@naver.com>
2024-06-08 14:20:42 +0900

    [#97] feat: 체험 등록페이지 수정

--- app/activities/register/page.tsx
+++ app/activities/register/page.tsx
@@ -3,8 +3,12 @@ import SideNavigationMenu from '@/components/commons/SideNavigationMenu';
 import Gnb from '@/components/commons/gnb/gnb';
 import Button from '@/components/commons/Button';
 import BaseInput from '@/components/input/BaseInput';
-import { useForm, FieldValues } from 'react-hook-form';
-import Selectbox from '@/components/commons/Selectbox';
+import {
+  useForm,
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+} from 'react-hook-form';
 import { useState } from 'react';
 import TextInput from '@/components/input/TextInput';
 import PriceInput from '@/components/input/PriceInput';
@@ -13,83 +17,144 @@ import TimeInput from '@/components/input/TimeInput';
 import Footer from '@/components/commons/Footer';
 import BannerImageInput from '@/components/input/BannerImageInput';
 import BioImageInput from '@/components/input/BioImageInput';
+import CategoryInput from '@/components/input/CategoryInput';
+import { useRouter } from 'next/navigation';
+
+interface ActivitiesData {
+  title: string;
+  description: string;
+  address: string;
+  price: number;
+  category: string;
+  schedules: any[];
+  bannerImageUrl: string;
+  subImageUrls: string[];
+}
+export type KeyActivitiesData = keyof ActivitiesData;
 
 export default function Registerpage() {
-  const [bannerFiles, setBannerFiles] = useState<File[]>([]);
-  const [bioFiles, setBioFiles] = useState<File[]>([]);
-
-  const methods = useForm<FieldValues>({
+  const [bannerImageUrl, setBannerImageUrl] = useState<string[]>([]);
+  const [subImageUrls, setSubImageUrls] = useState<string[]>([]);
+  console.log('bannerFiles', bannerImageUrl);
+  console.log('bioFiles', subImageUrls);
+  const router = useRouter();
+  const methods = useForm<ActivitiesData>({
     mode: 'onBlur',
+    defaultValues: {
+      title: '',
+      description: '',
+      address: '',
+      schedules: [],
+      price: 0,
+      category: '',
+      bannerImageUrl: '',
+      subImageUrls: [],
+    },
   });
-
   const {
     handleSubmit,
     control,
+    setValue,
     formState: { isValid },
   } = methods;
-  const [value, setValue] = useState();
 
-  const handlevalue = (value: any) => {
-    setValue(value);
+  const handlevalue = (id: keyof ActivitiesData, value: any) => {
+    setValue(id, value);
   };
 
-  const selectList = [
-    { value: '문화예술', label: '문화예술' },
-    { value: '식음료', label: '식음료' },
-    { value: '스포츠', label: '스포츠' },
-    { value: '투어', label: '투어' },
-    { value: '관광', label: '관광' },
-  ];
+  const onsubmit: SubmitHandler<ActivitiesData> = async (
+    data: ActivitiesData,
+  ) => {
+    if (bannerImageUrl.length === 0) {
+      return;
+    }
+    const formData = {
+      ...data,
+      price: Number(data.price),
+      bannerImageUrl: bannerImageUrl[0],
+      subImageUrls,
+    };
+    console.log('formData', formData);
+    try {
+      const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+      const accessToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQ2LCJ0ZWFtSWQiOiI0LTEzIiwiaWF0IjoxNzE3NzM5NTYxLCJleHAiOjE3MTg5NDkxNjEsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.arOO-urUgGHYxA2oQi48wQ7752KjRBGbT2M5RoUO7-I';
+      const response = await fetch(`${BASE_URL}/activities`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(formData),
+      });
+      // if (!response.ok) {
+      //   router.push('/activities');
+      // }
+    } catch (e) {
+      console.log('e');
+    }
+  };
 
   return (
-    <div className="bg-gray50 min-w-[350px]">
-      <Gnb />
-      <main className="flex justify-center min-h-[100vh] max-h-[100%] px-6 mobile:px-4 pt-[142px] pb-[72px] tablet:pt-[94px] mobile:pt-[94px] mb-[150px] mobile:mb-[100px]">
-        <div className="flex gap-6 w-[1200px] ">
-          <SideNavigationMenu />
-          <div className="flex flex-col flex-grow gap-6">
-            <div className="flex justify-between">
-              <p className="text-title text-black">내 체험 등록 </p>
-              <Button
-                width={120}
-                height={45}
-                fontSize={16}
-                textBold={true}
-                btnColor={'nomadBlack'}
-                textColor={'white'}
-                rounded={4}
-                style={{ cursor: 'pointer' }}
-                type="submit"
-              >
-                등록하기
-              </Button>
-            </div>
-            <BaseInput name="name" placeholder="제목" control={control} />
-            <Selectbox
-              options={selectList}
-              onSelect={handlevalue}
-              placeholder="카테고리"
-            />
-            <TextInput name="bio" placeholder="설명" control={control} />
-            <PriceInput
-              name="price"
-              placeholder="가격"
-              control={control}
-              labelName="가격"
-            />
-            <AddressInput />
-            <TimeInput
-              name="time"
-              placeholder="YY/MM/DD"
-              control={control}
-              labelName="예약 가능 시간대"
-            />
-            <BannerImageInput files={bannerFiles} setFiles={setBannerFiles} />
-            <BioImageInput files={bioFiles} setFiles={setBioFiles} />
+    <FormProvider {...methods}>
+      <div className="bg-gray50 min-w-[350px]">
+        <Gnb />
+        <main className="flex justify-center min-h-[100vh] max-h-[100%] px-6 mobile:px-4 pt-[142px] pb-[72px] tablet:pt-[94px] mobile:pt-[94px] mb-[150px] mobile:mb-[100px]">
+          <div className="flex gap-6 w-[1200px] ">
+            <SideNavigationMenu />
+            <form onSubmit={handleSubmit(onsubmit)}>
+              <div className="flex flex-col flex-grow gap-6">
+                <div className="flex justify-between">
+                  <p className="text-title text-black">내 체험 등록 </p>
+                  <Button
+                    width={120}
+                    height={45}
+                    fontSize={16}
+                    textBold={true}
+                    btnColor={'nomadBlack'}
+                    textColor={'white'}
+                    rounded={4}
+                    style={{ cursor: 'pointer' }}
+                    type="submit"
+                  >
+                    등록하기
+                  </Button>
+                </div>
+                <BaseInput name="title" placeholder="제목" control={control} />
+                <CategoryInput handlevalue={handlevalue} />
+                <TextInput
+                  name="description"
+                  placeholder="설명"
+                  control={control}
+                />
+                <PriceInput
+                  name="price"
+                  placeholder="가격"
+                  control={control}
+                  labelName="가격"
+                />
+                <AddressInput handlevalue={handlevalue} />
+                <TimeInput
+                  name="time"
+                  placeholder="YY/MM/DD"
+                  control={control}
+                  labelName="예약 가능 시간대"
+                  handlevalue={handlevalue}
+                />
+                <BannerImageInput
+                  files={bannerImageUrl}
+                  setFiles={setBannerImageUrl}
+                />
+                <BioImageInput
+                  files={subImageUrls}
+                  setFiles={setSubImageUrls}
+                />
+              </div>
+            </form>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    </FormProvider>
   );
 }
