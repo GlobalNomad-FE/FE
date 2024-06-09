f7666db73f8d288678cfe6068b607d0ba1cba57a
seungcar <ysjysj777@naver.com>
2024-06-10 07:11:37 +0900

    feat: 에러처리

--- components/MainBody/AllActivities.tsx
+++ components/MainBody/AllActivities.tsx
@@ -103,6 +103,10 @@ const AllActivities = ({ searchTerm, itemSize }: Props) => {
             />
           ))}
         </div>
+      ) : isError ? (
+        <div className="w-auto mainPcSize:w-[1200px] h-[384px] flex justify-center items-center">
+          데이터를 불러오는 것에 실패했습니다
+        </div>
       ) : (
         <div className="w-auto mainPcSize:w-[1200px] mx-0 text-center text-xl py-20">
           <p>조건에 맞는 체험이 없습니다.</p>
