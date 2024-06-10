6f5da0acc1776e3bb6d8da253f2f0d6e6b90f688
janyongs <wldud5023@naver.com>
2024-06-11 00:10:50 +0900

    [#120] feat : 체험상세페이지 쿼리

---
+++ apis/activities/useGetActivitiesDetail.ts
@@ -0,0 +1,22 @@
+'use client';
+import instance from '@/apis/axios';
+import { API } from '@/utils/constants/API';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { Activity } from './useGetActivities';
+
+interface ActivityDetail extends Activity {
+  subImages: [];
+}
+
+const getActivities = (activityId: number): Promise<ActivityDetail> => {
+  return instance.get(`${API.ACTIVITIE}/${activityId}`).then((res) => res.data);
+};
+
+export const useGetActivitiesDetail = (
+  activityId: number,
+): UseQueryResult<ActivityDetail> => {
+  return useQuery({
+    queryKey: ['activities', 'detail', activityId],
+    queryFn: () => getActivities(activityId),
+  });
+};
