abeb3d2c0f383a5bb26e7a7c7fbb37ec7001b592
seungcar <ysjysj777@naver.com>
2024-06-10 21:30:08 +0900

    feat: 버튼컴포넌트 호버기능 삭제

--- apis/axios.ts
+++ apis/axios.ts
@@ -5,42 +5,42 @@ const instance = axios.create({
   baseURL: BASE_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  if (config.headers.Authorization) return config;
+// instance.interceptors.request.use((config) => {
+//   if (config.headers.Authorization) return config;
 
-  const accessToken = localStorage.getItem('accessToken');
-  if (accessToken) {
-    config.headers['Authorization'] = `Bearer ${accessToken}`;
-  }
-  return config;
-});
+//   const accessToken = localStorage.getItem('accessToken');
+//   if (accessToken) {
+//     config.headers['Authorization'] = `Bearer ${accessToken}`;
+//   }
+//   return config;
+// });
 
-instance.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const originalRequest = error.config;
-    const refreshToken = localStorage.getItem('refreshToken');
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      refreshToken
-    ) {
-      const res = await instance.post(
-        '/auth/tokens',
-        {},
-        {
-          headers: { Authorization: `Bearer ${refreshToken}`, _retry: true },
-        },
-      );
-      const accessToken = res.data.accessToken;
-      const nextRefreshToken = res.data.refreshToken;
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', nextRefreshToken);
-      originalRequest._retry = true;
+// instance.interceptors.response.use(
+//   (res) => res,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     const refreshToken = localStorage.getItem('refreshToken');
+//     if (
+//       error.response?.status === 401 &&
+//       !originalRequest._retry &&
+//       refreshToken
+//     ) {
+//       const res = await instance.post(
+//         '/auth/tokens',
+//         {},
+//         {
+//           headers: { Authorization: `Bearer ${refreshToken}`, _retry: true },
+//         },
+//       );
+//       const accessToken = res.data.accessToken;
+//       const nextRefreshToken = res.data.refreshToken;
+//       localStorage.setItem('accessToken', accessToken);
+//       localStorage.setItem('refreshToken', nextRefreshToken);
+//       originalRequest._retry = true;
 
-      return instance(originalRequest);
-    }
-    return Promise.reject(error);
-  },
-);
+//       return instance(originalRequest);
+//     }
+//     return Promise.reject(error);
+//   },
+// );
 export default instance;
