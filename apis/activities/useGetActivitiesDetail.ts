a58b832bfbd7b5a97431304f638657767efcdb6a
janyongs <wldud5023@naver.com>
2024-06-11 20:17:29 +0900

    feat : 체험상세 쿼리 수정

--- apis/activities/useGetActivitiesDetail.ts
+++ apis/activities/useGetActivitiesDetail.ts
@@ -3,9 +3,10 @@ import instance from '@/apis/axios';
 import { API } from '@/utils/constants/API';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { Activity } from './useGetActivities';
+import { BioImageType } from '@/components/activitie/activitieTitile/ActivitieTitle';
 
 export interface ActivityDetail extends Activity {
-  subImages: [];
+  subImages: BioImageType[];
 }
 
 const getActivities = (activityId: number): Promise<ActivityDetail> => {
