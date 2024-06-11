5d5efe80103a5f712bb3f41fb99a006688c28b68
gaem-gaem <kkmm080399@gmail.com>
2024-06-11 19:53:49 +0900

    알림 창 testPage

---
+++ app/testNotifications/page.tsx
@@ -0,0 +1,11 @@
+import MyNotifications from '@/components/commons/MyNotifications';
+
+const TestNotifications = () => {
+  return (
+    <div>
+      <MyNotifications />
+    </div>
+  );
+};
+
+export default TestNotifications;
