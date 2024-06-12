b5353d4fe2323ef981c8ef806fd51061a54830ba
gaem-gaem <kkmm080399@gmail.com>
2024-06-12 21:39:29 +0900

    feat: 반응형 적용

--- components/commons/MyNotifications.tsx
+++ components/commons/MyNotifications.tsx
@@ -60,7 +60,7 @@ const MyNotifications = ({ onClose }: MyNotificationsProps) => {
 
   return (
     <div
-      className="w-[368px] px-5 py-6 rounded-[10px] border border-[#CBC9CF] bg-green400 text-black200"
+      className="w-[368px] px-5 py-6 rounded-[10px] border border-[#CBC9CF] bg-green400 text-black200 mobile:w-full mobile:h-full mobile:fixed mobile:top-0 mobile:right-0 mobile:py-10 mobile:rounded-none mobile:overflow-scroll"
       style={{ boxShadow: '0px 2px 8px 0px rgba(120, 116, 134, 0.25)' }}
     >
       <div className="flex justify-between items-center mb-4">
