8c2cc5e64f0cb851e0f18b94778de1811aeb8938
janyongs <wldud5023@naver.com>
2024-06-14 17:55:48 +0900

    [#143] feat: 체험리스트, 내예약내역 스켈레톤 추가

---
+++ components/skeleton/MyActivitiyExperienceSkeleton.tsx
@@ -0,0 +1,16 @@
+export default function MyActivitiyExperienceSkeleton() {
+  return (
+    <div className="animate-pulse mt-4 max-w-[792px] h-[204px] tablet:h-[156px] mobile:h-[128px] rounded-[24px] flex bg-white">
+      <div className="min-w-[204px] h-[204px] tablet:min-w-[156px] tablet:h-[156px] mobile:min-w-[128px] mobile:h-[128px] relative bg-gray-300 rounded-l-[24px]"></div>
+      <div className="flex flex-col justify-between w-full p-6 tablet:p-[12px] mobile:p-[9px]">
+        <div>
+          <p className="bg-gray-300 h-4 w-16 mb-3 rounded-xl"></p>
+          <p className="bg-gray-300 h-6 tablet:h-5 mobile:h-4 w-36 mb-2 tablet:mb-0 mobile:mb-[5px] rounded-xl"></p>
+        </div>
+        <div className="h-10 mobile:h-[32px] flex justify-between mt-4 tablet:mt-[12px] mobile:mt-[5px] items-center mobile:mr-[3px]">
+          <p className="bg-gray-300 h-6 tablet:h-5 mobile:h-4 w-36 rounded-xl"></p>
+        </div>
+      </div>
+    </div>
+  );
+}
