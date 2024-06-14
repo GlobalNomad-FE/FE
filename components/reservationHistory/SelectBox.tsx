43521db12dc8ecac0e66576bbb3a091ee429e088
Yeojin Yoon <roseiris1001@gmail.com>
2024-06-15 06:34:06 +0900

    feat : 데이터에 띠리 selectBox 내부에 선택 가능하도록 수정

--- components/reservationHistory/SelectBox.tsx
+++ components/reservationHistory/SelectBox.tsx
@@ -1,18 +1,54 @@
 import Image from 'next/image';
+import { Activity, MyActivitieType } from '@/types/myActivitiesType';
+import { useState } from 'react';
 
-const SelectBox = () => {
+interface SelectBoxProps {
+  activities: Activity[];
+  onSelect: (id: number) => void;
+}
+
+const SelectBox: React.FC<SelectBoxProps> = ({ activities, onSelect }) => {
+  const [selectActivity, setSelectActivity] = useState<Activity | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (activity: Activity) => {
+    setSelectActivity(activity);
+    setIsOpen(false);
+    onSelect(activity.id); // 선택된 활동의 id 값을 상위 컴포넌트로 전달
+  };
+
   return (
-    <div className="w-[800px] tablet:w-[429px] mobile:w-full h-[48px] mt-[42px] px-4 py-3 border border-gray500 rounded-[4px] bg-white relative flex justify-between">
-      <p className="bg-white w-[45px] px-1 text-[14px] bottom-10 absolute">
-        체험명
-      </p>
-      <p className="text-[16px]">함께 배우면 즐거운 스트릿 댄스!</p>
-      <Image
-        src="./icons/arrow_down.svg"
-        alt="아래 화살표 아이콘"
-        width={24}
-        height={24}
-      />
+    <div className="relative w-[800px] tablet:w-[429px] mobile:w-full h-[48px] mt-[42px]">
+      <div
+        className="px-4 py-3 border border-gray500 rounded-[4px] bg-white flex justify-between cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p className="bg-white w-[45px] px-1 text-[14px] bottom-10 absolute">
+          체험명
+        </p>
+        <p className="text-[16px]">
+          {selectActivity ? selectActivity.title : '체험을 선택하세요'}
+        </p>
+        <Image
+          src="./icons/arrow_down.svg"
+          alt="아래 화살표 아이콘"
+          width={24}
+          height={24}
+        />
+      </div>
+      {isOpen && (
+        <ul className="absolute z-10 w-full bg-white border border-gray500 rounded-[4px] mt-1 max-h-60 overflow-auto">
+          {activities.map((activity) => (
+            <li
+              key={activity.id}
+              className="px-4 py-2 text-[16px] cursor-pointer hover:bg-gray-200"
+              onClick={() => handleSelect(activity)}
+            >
+              {activity.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
