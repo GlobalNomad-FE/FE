178a0d70874716d74081aaa63513e5b6a658a691
Yeojin Yoon <roseiris1001@gmail.com>
2024-06-16 17:09:06 +0900

    feat : 예약 현황 미존재 시 보여주는 부분 추가

--- app/reservationHistory/page.tsx
+++ app/reservationHistory/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Image from 'next/image';
 import Gnb from '@/components/commons/gnb/gnb';
 import Footer from '@/components/commons/Footer';
 import Calendar from '@/components/reservationHistory/Calendar';
@@ -10,7 +10,7 @@ import React, { useState } from 'react';
 export default function Page() {
   //TODO: 내 체험 리스트 데이터 가져오기
   //TODO: 내 체험 리스트 목업
-  const myActivityes = [
+  let myActivityes = [
     {
       id: 1154,
       userId: 409,
@@ -66,7 +66,7 @@ export default function Page() {
 
   //TODO: selectedActivityId로 월별 예약 리스트 데이터 가져오기
   //TODO: 월별 예약 리스트 목업 데이터
-  const MonthReservations = [
+  let MonthReservations = [
     {
       date: '2024-06-01',
       reservations: {
@@ -117,18 +117,38 @@ export default function Page() {
     },
   ];
 
+  myActivityes = [];
+
   return (
     <div className="text-black200 bg-gray50">
       <Gnb />
       <div className="pt-[142px] tablet:pt-[94px] mobile:pt-[94px] w-full flex justify-center">
         <SideNavigationMenu />
         <div className="w-[800px] pl-[24px] tablet:w-[429px] mobile:w-[326px] mobile:px-4 text-[32px] mb-[142px] tablet:mb-[128px]">
-          <p className="w-[800px] mobile:w-full font-bold">예약현황</p>
-          <SelectBox myActivityes={myActivityes} onSelect={handleSelect} />
-          <Calendar
-            MonthReservations={MonthReservations}
-            selectedActivityId={selectedActivityId}
-          />
+          <p className="w-[800px] mobile:w-full font-bold">예약 현황</p>
+          {myActivityes.length > 0 ? (
+            <>
+              <SelectBox myActivityes={myActivityes} onSelect={handleSelect} />
+              <Calendar
+                MonthReservations={MonthReservations}
+                selectedActivityId={selectedActivityId}
+              />
+            </>
+          ) : (
+            <div className="h-[1133px] pt-[111px] tablet:pt-[100px] mobile:pt-[80px] flex flex-col items-center">
+              <div className="w-[130px] tablet:w-[105px] mobile:w-[105px] h-[177px] tablet:h-[148px] mobile:h-[148px] overflow-hidden relative">
+                <Image
+                  src="/icons/empty.svg"
+                  alt="예약 현황 미존재를 나타내는 이미지"
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+              <p className="font-medium text-gray500 text-[24px] pt-[50px] tablet:pt-[36px]">
+                아직 등록한 체험이 없어요.
+              </p>
+            </div>
+          )}
         </div>
       </div>
       <Footer />
