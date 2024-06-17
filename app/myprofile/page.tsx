d5b1533b61e7c46ac5cc7fb70d5110cc3e283766
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-06-17 19:29:54 +0900

    style: 폼 꾸미기

--- app/myprofile/page.tsx
+++ app/myprofile/page.tsx
@@ -3,16 +3,22 @@
 import React, { useState } from 'react';
 import MyPageForm from '@/components/myProfile/MyPageForm';
 import SideNavigationMenu from '@/components/commons/SideNavigationMenu';
+import Gnb from '@/components/commons/gnb/gnb';
 
 const MyPage = () => {
   const [uploadedImage, setUploadedImage] = useState<string | null>(null);
   return (
-    <div className="flex gap-6 justify-center bg-[#FAFAFA] pt-[65px]">
-      <SideNavigationMenu />
-      <MyPageForm
-        uploadedImage={uploadedImage}
-        setUploadedImage={setUploadedImage}
-      />
+    <div className="flex gap-6 justify-center bg-[#FAFAFA] ">
+      <Gnb />
+      <div className="flex justify-center min-h-[100vh] max-h-[100%] px-6 bg-gray50 pt-[142px] pb-[72px]">
+        <div className="flex gap-6 w-[1200px]">
+          <SideNavigationMenu />
+          <MyPageForm
+            uploadedImage={uploadedImage}
+            setUploadedImage={setUploadedImage}
+          />
+        </div>
+      </div>
     </div>
   );
 };
