ea96e60cbcf5bdad348a1423807e0021a39ba7c8
janyongs <wldud5023@naver.com>
2024-06-20 22:16:10 +0900

    [#160] feat :  프로필이미지 파일 추가 수정

--- components/myProfile/ProfileImage.tsx
+++ components/myProfile/ProfileImage.tsx
@@ -1,81 +1,122 @@
-import React, { ChangeEvent } from 'react';
+import React, { useState } from 'react';
 import uploadProfileImage from '@/apis/user/uploadProfileImage';
 import Image from 'next/image';
-import InformationNoImage from './InformationImage';
-import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useUserProfile from '@/hooks/useUserProfile';
 
-const ProfileImage = ({
-  nickname,
-  profileImageUrl,
-  uploadedImage = null,
-  setUploadedImage = () => null,
-}: {
-  nickname: string;
-  profileImageUrl: string;
-  uploadedImage?: string | null;
-  setUploadedImage?: React.Dispatch<React.SetStateAction<string | null>>;
-}) => {
-  useEffect(() => {
-    const storedImageUrl = Cookies.get('uploadedImage');
-    if (storedImageUrl) {
-      setUploadedImage(storedImageUrl);
-    }
-  }, [setUploadedImage]);
+interface Props {
+  url: (url: string) => void;
+}
 
-  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      try {
-        const imageData = await uploadProfileImage(file);
-        setUploadedImage(imageData.profileImageUrl);
-        Cookies.set('uploadedImage', imageData.profileImageUrl);
-      } catch (error) {
-        console.error('Failed to upload image:', error);
-      }
-    }
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative w-12 h-12 ">
+        {/* 도넛 모양 배경 */}
+        <div className="absolute opacity-45 top-0 left-0 w-full h-full border-[6px] border-gray-300 rounded-full"></div>
+        {/* 회전하는 검정색 1/4 도넛 */}
+        <div className="absolute top-0 left-0 w-full h-full animate-spin">
+          <div className="w-full h-full border-[6px] opacity-75 border-transparent border-t-green200 border-r-green400 border-l-green400 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProfileImage = ({ url }: Props) => {
+  const [modify, setModify] = useState(false);
+  const [uploadedImage, setUploadedImage] = useState<string[]>([]);
+  const queryClient = useQueryClient();
+  const { user } = useUserProfile();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const uploadImagePostMutation = useMutation({
+    mutationKey: ['user', 'profileImage'],
+    mutationFn: (newProfileImage: File) => uploadProfileImage(newProfileImage),
+  });
+
+  const handleFileChange = (newProfileImage: File) => {
+    uploadImagePostMutation.mutate(newProfileImage, {
+      onSuccess: (data: { profileImageUrl: string }) => {
+        setUploadedImage((prev) => [...prev, data.profileImageUrl]),
+          url(data.profileImageUrl),
+          queryClient.invalidateQueries({ queryKey: ['user'] });
+      },
+    });
   };
 
-  const handlePenClick = () => {
-    const fileInput = document.getElementById('file-input');
-    if (fileInput) fileInput.click();
-  };
-
-  if (!profileImageUrl && !uploadedImage) {
-    return (
-      <InformationNoImage
-        nickname={nickname}
-        setUploadedImage={setUploadedImage}
-        uploadedImage={uploadedImage}
-      />
-    );
+  function handlePenClick(e: React.ChangeEvent<HTMLInputElement>) {
+    let selectedFiles = [] as File[];
+    if (e.type === 'change') {
+      const inputEl = e.target as HTMLInputElement;
+      selectedFiles = inputEl.files ? Array.from(inputEl.files) : [];
+    }
+    const addFile = selectedFiles?.[0];
+    if (addFile) {
+      setModify(true);
+      handleFileChange(addFile);
+    }
   }
 
+  const renderProfileImage = () => {
+    if (!user) {
+      return (
+        <div className="flex items-center justify-center w-full h-full">
+          <Spinner />
+        </div>
+      );
+    }
+    return modify ? (
+      <>
+        {uploadedImage.map((file) => (
+          <Image
+            src={file}
+            fill
+            alt={file}
+            placeholder="blur"
+            blurDataURL={file}
+            object-fit="contain"
+            key={`${file}_key`}
+          />
+        ))}
+      </>
+    ) : (
+      <Image
+        src={user.profileImageUrl}
+        fill
+        alt={user.profileImageUrl}
+        placeholder="blur"
+        blurDataURL={user.profileImageUrl}
+        object-fit="contain"
+      />
+    );
+  };
+
   return (
-    <>
+    <div onClick={() => inputRef.current?.click()} className="relative">
       <div
-        className="relative w-40 h-40 shrink-0 rounded-full shadow-md bg-cover bg-no-repeat bg-center"
-        style={{
-          backgroundImage: `url(${uploadedImage || profileImageUrl})`,
-          backgroundColor: '#E3E5E8',
-          backgroundSize: 'contain',
-        }}
+        className="w-[160px] h-[160px] rounded-[160px] bg-gray200 overflow-hidden relative"
+        style={{ boxShadow: '0px 4px 16px 0px rgba(0, 0, 0, 0.08)' }}
       >
-        <input
-          id="file-input"
-          type="file"
-          onChange={handleFileChange}
-          style={{ display: 'none' }}
-          accept="image/*"
-        />
+        {renderProfileImage()}
       </div>
-      <div
-        className="absolute p-[10px] w-22 h-22  items-start bottom-0 right-3 z-10 rounded-full cursor-pointer"
-        onClick={handlePenClick}
-      >
-        <Image width={44} height={44} src="/icons/penIcon.svg" alt="penIcon" />
-      </div>
-    </>
+      <Image
+        width={44}
+        height={44}
+        src="/icons/penIcon.svg"
+        alt="penIcon"
+        className="absolute bottom-0 right-0 z-10"
+      />
+      <input
+        id="file-input"
+        type="file"
+        ref={inputRef}
+        style={{ display: 'none' }}
+        accept="image/*"
+        onChange={handlePenClick}
+      />
+    </div>
   );
 };
 
