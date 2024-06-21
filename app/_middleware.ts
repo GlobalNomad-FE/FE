675ae8846ffefa2101ced8116391f8f4c9b83149
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-06-21 14:37:44 +0900

    미들웨어

--- app/_middleware.ts
+++ app/_middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // 쿠키에서 accessToken 가져오기
-  const accessToken = request.cookies.get('accessToken');
+  // 서버 측에서 쿠키 가져오기
+  let accessToken = request.cookies.get('accessToken');
   console.log('Access Token:', accessToken);
 
   // accessToken이 없는 경우 로그인 페이지로 리다이렉트
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 
 // 특정 경로(인가가 필요한 페이지)로 요청했을 때 쿠키에 토큰이 없으면 로그인 페이지로 redirect됩니다.
 export const config = {
-  matcher: ['/myprofile/:path*'],
+  matcher: ['/app/myprofile'],
 };
