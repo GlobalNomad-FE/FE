b87dd986a75faad435c6c7362b8276be8d49a3ea
janyongs <wldud5023@naver.com>
2024-06-21 17:18:45 +0900

    이미지 등록 수정

--- apis/UploadImage.ts
+++ apis/UploadImage.ts
@@ -1,22 +1,16 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const accessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQ2LCJ0ZWFtSWQiOiI0LTEzIiwiaWF0IjoxNzE3NzM5NTYxLCJleHAiOjE3MTg5NDkxNjEsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.arOO-urUgGHYxA2oQi48wQ7752KjRBGbT2M5RoUO7-I';
+import instance from './axios';
+import { toast } from 'react-toastify';
 
 export async function uploadImagePost(imageFile: File) {
   const formData = new FormData();
 
   formData.append('image', imageFile);
 
-  const response = await fetch(`${BASE_URL}/activities/image`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-    body: formData,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to upload the image.');
+  try {
+    const response = instance.post('/activities/image', formData);
+    return (await response).data;
+  } catch (error) {
+    toast.error('Error Upload Image');
+    throw error;
   }
-  return await response.json();
 }
