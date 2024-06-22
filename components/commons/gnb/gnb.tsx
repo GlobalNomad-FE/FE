727cd17b33588b3b6fd5d2cebfe0833d83df8c37
seungcar <ysjysj777@naver.com>
2024-06-22 13:40:34 +0900

    feat: 로딩처리

--- components/commons/gnb/gnb.tsx
+++ components/commons/gnb/gnb.tsx
@@ -1,16 +1,13 @@
-'use client';
-
-import { useOutsideClick } from '@/hooks/useOutsideClick';
-import { useToggleButton } from '@/hooks/useToggleButton';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
 import Avatar from '@/components/commons/avatar/avatar';
 import DropdownMenu from '../dropdownMenu/DropdownMenu';
 import Cookies from 'js-cookie';
 import MyNotifications from '../myNotifications/MyNotifications';
 import { useGetProfile } from '@/apis/user/useGetProfile';
+import { useToggleButton } from '@/hooks/useToggleButton';
 
 export default function GNB() {
   const router = useRouter();
@@ -19,19 +16,27 @@ export default function GNB() {
 
   const { isToggle: isDropdownOpen, handleToggleClick: isDropdownOpenToggle } =
     useToggleButton();
-
   const ref = useRef<HTMLButtonElement>(null);
 
-  useOutsideClick(ref, isDropdownOpen, isDropdownOpenToggle);
+  useEffect(() => {
+    const accessToken = Cookies.get('accessToken');
+    const refreshToken = Cookies.get('refreshToken');
+    if (accessToken && refreshToken) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, []);
 
+  // 로그아웃 처리
   const handleLogout = () => {
     Cookies.remove('accessToken');
     Cookies.remove('refreshToken');
     if (!Cookies.get('accessToken')) {
       isDropdownOpenToggle();
       router.push('/login');
+      window.location.reload();
     }
-    window.location.reload();
   };
 
   const MyMenuList = [
@@ -68,18 +73,6 @@ export default function GNB() {
     },
   ];
 
-  useEffect(() => {
-    if (Cookies.get('accessToken') && Cookies.get('refreshToken')) {
-      setAuth(true);
-      return;
-    }
-    setAuth(false);
-  }, []);
-
-  if (isPending) {
-    return null;
-  }
-
   return (
     <div className="fixed top-0 text-black bg-white w-full h-[70px] border-b border-gray200 flex z-40">
       <div className="max-w-[1248px] w-full mx-auto flex items-center justify-between px-[24px]">
@@ -92,21 +85,21 @@ export default function GNB() {
           />
         </Link>
         <div>
-          {!Auth ? (
+          {isPending || !Auth ? (
             <div className="flex gap-[25px] relative text-body2-regular font-medium text-black200">
               <Link href="/login">로그인</Link>
               <Link href="/signup">회원가입</Link>
             </div>
           ) : (
-            <div className=" flex items-center gap-[25px] static">
+            <div className="flex items-center gap-[25px] static">
               <MyNotifications />
               <div className="w-[1px] h-[22px] bg-gray200" />
-              <div className=" flex relative gap-10">
+              <div className="flex relative gap-10">
                 <div className="flex w-fit-content gap-[16px]">
                   <button
                     onClick={isDropdownOpenToggle}
                     ref={ref}
-                    className="flex text-[16px] items-center body1-regular gap-[10px] text-nomad-black "
+                    className="flex items-center text-[16px] body1-regular gap-[10px] text-nomad-black"
                   >
                     <Avatar profileImageUrl={MyInfoData?.profileImageUrl} />
                     {MyInfoData?.nickname}
