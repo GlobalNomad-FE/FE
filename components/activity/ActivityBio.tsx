0eaa940074ace0240b7258d16353a1c16401a481
janyongs <wldud5023@naver.com>
2024-06-23 17:54:55 +0900

    체험설명 줄바꿈 적용

--- components/activity/ActivityBio.tsx
+++ components/activity/ActivityBio.tsx
@@ -4,7 +4,9 @@ export default function ActivityBio({ description }: { description: string }) {
   return (
     <div className="border-t border-gray200 py-[40px] flex flex-col gap-[16px]">
       <p className="text-h3-bold text-nomad-black">체험 설명</p>
-      <p className="text-body1-regular text-nomad-black">{description}</p>
+      <p className="text-body1-regular text-nomad-black whitespace-pre-wrap">
+        {description}
+      </p>
     </div>
   );
 }
